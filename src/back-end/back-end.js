1a597a66c5cad02a2f84df3351efa38f48e8bfe3
Tina <tinzzy.cracked@yahoo.com>
2023-01-02 14:07:14 -0500

    catching error and data

--- src/back-end/back-end.js
+++ src/back-end/back-end.js
@@ -4,10 +4,20 @@ const app = express();
 const MySqlConnection = require('./MySqlConnection');
 const connection = MySqlConnection.INSTANCE();
 
-app.listen(8888)
-
 app.get('/connection-is-ok', async function (req, res) {
     let isOK = await connection.connect(req.query)
-    res.send({isOK})
+    res.send({ isOK })
 })
 
+app.get('/execute-sql', async function (req, res) {
+    try {
+        let result = await connection.executeSql(req.query);
+        res.send(result);
+    } catch (error) {
+        res.send({ error: error.message, row: [] });
+    }
+})
+
+
+
+app.listen(8888)
