676b6ba10e434ba1ae0cd79f1c64ae2b220ae9d3
Tina <tinzzy.cracked@yahoo.com>
2023-01-03 10:59:41 -0500

    display data from MySQL in a table

--- src/front-end/DisplayData.js
+++ src/front-end/DisplayData.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import Box from '@mui/material/Box';
+
 import { shared } from './shared';
 
 class DisplayData extends React.Component {
@@ -7,7 +9,9 @@ class DisplayData extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+
         }
+        this.callDisplayData = this.callDisplayData.bind(this);
         shared.callDisplayData = this.callDisplayData;
     }
 
@@ -19,8 +23,26 @@ class DisplayData extends React.Component {
 
     render() {
         return (
-            <>
-            </>
+            <Box>
+                {this.state.data &&
+                    <table style={{ border: 'solid 1px black', padding: 20 }}>
+                        <tbody>
+                            <tr key={"header"} margin="20px">
+                                {Object.keys(this.state.data[0]).map((e) => (
+                                    <th>{e}</th>
+                                ))}
+                            </tr>
+                            {this.state.data.map((e, i) => (
+                                <tr key={i} style={{ border: 'solid 1px black' }} margin="20px">
+                                    {Object.values(e).map((val) => (
+                                        <td>{val}</td>
+                                    ))}
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                }
+            </Box>
         );
     }
 }
