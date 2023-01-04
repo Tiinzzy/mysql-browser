fc4a91e88c098f75879c426122b44609f289c615
Tina <tinzzy.cracked@yahoo.com>
2023-01-04 11:58:04 -0500

    created sidebar for the available tables in database to display

--- src/front-end/MainPage.js
+++ src/front-end/MainPage.js
@@ -1,9 +1,12 @@
 import * as React from 'react';
 import { useState } from 'react';
+
+import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
 
 import ConnectionInfo from './ConnectionInfo';
 import QueryWindow from './QueryWindow';
+import GetSqlTables from './GetSqlTables';
 
 export default function MainPage() {
     const [queryOk, setQueryOk] = useState(false);
@@ -13,12 +16,18 @@ export default function MainPage() {
     }
 
     return (
-        <div style={{ margin: 10 }}>
-            <ConnectionInfo callSetQueryOk={callSetQueryOk} />
+        <Box style={{ display: 'flex', flexDirection: 'row', marginTop: 10 }}>
+            <Box style={{ border: 'solid 1px black', display: 'flex', flexDirection: 'column', marginLeft: 20, marginTop: 28, height: '340px' }}>
+                <GetSqlTables />
+            </Box>
+            <div style={{ margin: 10 }}>
+                <ConnectionInfo callSetQueryOk={callSetQueryOk} />
 
-            <Divider sx={{ mt: 2, mb: 2 }} />
+                <Divider sx={{ mt: 2, mb: 2 }} />
 
-            {queryOk && <QueryWindow />}
-        </div>
+                {queryOk && <QueryWindow />}
+            </div>
+        </Box>
+
     );
 }
