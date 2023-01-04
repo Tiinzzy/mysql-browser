04911ac013bba574e8fcff1ce092d894f81011ee
Tina <tinzzy.cracked@yahoo.com>
2023-01-04 13:45:57 -0500

    added check box so dialog doesn't open every time button submits

--- src/front-end/DialogContent.js
+++ src/front-end/DialogContent.js
@@ -3,27 +3,35 @@ import React from "react";
 import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
 import DialogTitle from "@mui/material/DialogTitle";
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
 
 class DialogContent extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            handleCloseDialog: props.handleCloseDialog
+            handleCloseDialog: props.handleCloseDialog,
+            checkBox: false
         }
         this.closeDialog = this.closeDialog.bind(this);
+        this.handleCheckBox = this.handleCheckBox.bind(this);
     }
 
     closeDialog(e) {
-        this.state.handleCloseDialog();
+        let checked = { message: 'check-box', status: this.state.checkBox }
+        this.state.handleCloseDialog(checked);
     }
 
+    handleCheckBox(e) {
+        this.setState({ checkBox: e.target.checked });
+    }
 
     render() {
         return (
             <Box>
                 <DialogTitle style={{ display: 'flex', justifyContent: 'space-between' }}>
-                    <Box style={{fontWeight:'bold'}}>Important</Box>
+                    <Box style={{ fontWeight: 'bold' }}>Important</Box>
                     <Box style={{ border: 'solid 1px black', height: 30, width: 30, textAlign: 'center', cursor: 'pointer' }} onClick={(e) => this.closeDialog()}>X</Box>
                 </DialogTitle>
                 <Divider sx={{ mb: 2 }} />
@@ -39,6 +47,9 @@ class DialogContent extends React.Component {
                     <p>
                         *Example: <span style={{ marginLeft: 10 }}> SELECT * FROM your_table LIMIT (2000, 30)</span>
                     </p>
+                </Box>
+                <Box style={{ marginLeft: 24, marginBottom: 25, marginRight: 25 }}>
+                    <FormControlLabel control={<Checkbox checked={this.state.checkBox} onChange={(e) => this.handleCheckBox(e)} />} label="Dont Show This Message Again" />
                 </Box>
             </Box>
         );
