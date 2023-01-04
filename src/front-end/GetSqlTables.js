41d777c573ab2d898471f667b5aa85bc2c1e04d3
Tina <tinzzy.cracked@yahoo.com>
2023-01-04 13:46:18 -0500

    mapped available tables from MySQL

--- src/front-end/GetSqlTables.js
+++ src/front-end/GetSqlTables.js
@@ -9,18 +9,27 @@ const backend = BackEndConnection.INSTANCE();
 
 export default function GetSqlTables() {
     const [sqlMsg, setSqlMsg] = useState('show tables');
+    const [tables, setTables] = useState(null);
 
     async function getSqlTables() {
         let result = await backend.getSqlTables({ sqlMsg });
-        console.log(result.rows)
+        setTables(result.rows);
         if (result.error) {
             console.log(result.error);
         }
     }
 
     return (
-        <Box styel={{display:'flex' }}>
-            <Button style={{ marginTop: 15, marginLeft: 15, marginRight: 15 }} variant="outlined" onClick={() => getSqlTables()}>Get Tables</Button>
+        <Box styel={{ display: 'flex' }}>
+            <Button style={{ marginTop: 15, marginLeft: 15, marginRight: 15 }} variant="text" onClick={() => getSqlTables()}>Availabe Tables</Button>
+            {tables !== null && tables.map((e, i) =>
+                <ul key={i}>
+                    {Object.values(e).map((val, j) => (
+                        <li key={j}>{val}
+                        </li>
+                    ))}
+                </ul>
+            )}
         </Box>
     );
 
